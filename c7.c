9f01b19bedc9795eb4ed1c5b196858847efd42e4
sezallagwal <sezal1470@gmail.com>
2024-03-02 21:11:01 +0530

    operators

---
+++ c7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    int a;
+    float b;
+    a= 34;
+    b= 6.56;
+
+    printf("a + b = %f\n", a+b);
+    printf("a - b = %f\n", a-b);
+    printf("a * b = %f\n", a*b);
+    printf("a / b = %f\n", a/b);
+
+    return 0;
+}
