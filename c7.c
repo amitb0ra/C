39fb9f2c27a67d945a3fc74219a5ba4870c06438
sezallagwal <sezal1470@gmail.com>
2024-03-02 21:12:43 +0530

    Update c7.c

--- c7.c
+++ c7.c
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 #include <stdio.h>
+=======
+#include<stdio<h>
+>>>>>>> Stashed changes
 
 int main()
 {
     /* code */
+<<<<<<< Updated upstream
     int a;
     float b;
     a= 34;
@@ -13,5 +18,9 @@ int main()
     printf("a * b = %f\n", a*b);
     printf("a / b = %f\n", a/b);
 
+=======
+    int a, b;
+    
+>>>>>>> Stashed changes
     return 0;
 }
