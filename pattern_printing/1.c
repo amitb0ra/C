2736c16e0c9c10836ca7df2b31c6a55f76ef7c9f
amitb0ra <info.amitsinghbora@gmail.com>
2024-03-04 21:26:48 +0530

    Update 1.c

--- pattern_printing/1.c
+++ pattern_printing/1.c
@@ -12,6 +12,14 @@ Print a Half Pyramid:
 
 int main()
 {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
