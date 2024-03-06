3b93534392c004c3a19e8b8603974b5fcfffbbef
amitb0ra <info.amitsinghbora@gmail.com>
2024-03-06 17:56:19 +0530

    Update 2.c

--- pattern_printing/2.c
+++ pattern_printing/2.c
@@ -12,6 +12,17 @@ Print a Right-angled Triangle Pattern:
 
 int main()
 {
+    int height = 4;
+
+    for (int i = 1; i <= height; i++)
+    {
+        printf("%*s", height - i, "");
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
