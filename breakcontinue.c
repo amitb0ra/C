af2195615ad1ef82485a8223db6aa2c4551bf7b1
sezallagwal <sezal1470@gmail.com>
2024-03-15 20:27:56 +0530

    Update breakcontinue.c

--- breakcontinue.c
+++ breakcontinue.c
@@ -6,11 +6,32 @@ int main()
     for (i = 0; i < 10; i++)
     {
         printf("%d\nEnter your age\n", i);
+<<<<<<< Updated upstream
         scanf("%d\n", &age);
         if (age > 10)
         {
             break;
         }
+=======
+        scanf("%d", &age);
+        //     if (age > 10)
+        //     {
+        //         break;
+        //     }
+
+        if (age > 10)
+        {
+            continue;
+        }
+
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+        printf("we've not come across continue statement");
+>>>>>>> Stashed changes
     }
 
     return 0;
