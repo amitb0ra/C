9f878fe2109fe9ba9640eddf365c32d7fca1c29f
sezallagwal <sezal1470@gmail.com>
2024-03-15 20:29:35 +0530

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// typecasting converting one datatype in another
+// syntax
+// (type) value;
+
+int main()
+{
+    int a = 3;
+    float b = (float)54 / 5;
+    printf("the value of a is %f\n", b);
+    return 0;
+}
